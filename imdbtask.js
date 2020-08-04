c4d1b6d2b44d01818a4d7261be254dc89b1128cf
Naveed Sayyed <sayyednaveed107@gmail.com>
2020-08-04 19:09:02 +0530

    Fetch and Async/Await and IMDB

---
+++ imdbtask.js
@@ -0,0 +1,54 @@
+//700bb029
+//omdbAPI url: http://www.omdbapi.com/?apikey=700bb029&t
+
+let getMovies = async () =>{
+    try{
+        let x = document.getElementById('movieName').value
+        let apiKey = '700bb029'
+        let url = `http://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(x)}&r=json`
+
+        let response = await fetch(url)
+        let movieData = await response.json()
+        layout(movieData)
+
+    }catch(error){
+        let movieContainererr = document.querySelector('#movieData')
+        movieContainererr.innerHTML = '<div><h1>Kindly Enter the Correct Movie Name</h1></div>'
+    }
+}
+
+
+let layout =(x) =>{
+
+    let movieContainer = document.querySelector('#movieData')
+
+    movieContainer.innerHTML = 
+    `<div class="card  cardDet">
+        <div class="card-body" style = "padding: 20px 0">
+            <div class ="padder">
+                <h3 class="card-title">${x.Title} (${x.Year})</h3>
+                <p>${x.Rated} | ${x.Runtime} | ${x.Genre} | ${x.Released} | IMDB - ${x.Ratings[0].Value} <i class="fa fa-star" aria-hidden="true"></i></p>
+            </div>
+            <img src=${x.Poster} class="card-img-top" alt="..." style = "width:100%;height: 40rem; margin-bottom: 10px">
+        </div>
+        <div class="card-body" style = "padding: 0 15px 10px 15px">
+            <p class="card-text">${x.Plot}</p>
+            <h5 class="card-text">Director: ${x.Director}</h5>
+            <h5 class="card-text">Stars: ${x.Actors}</h5>
+            <h5 class="card-text">Languages: ${x.Language}</h5>
+        </div>
+    </div>`
+}
+
+
+/* 
+
+ 
+ 
+Rotten tomatoes = Ratings[1].value 
+Runtime
+Actors
+Director
+Genre
+Language
+plot*/
---
+++ taskpromise.js
@@ -0,0 +1,72 @@
+
+let makelayout = () => {
+    let url = 'https://restcountries.eu/rest/v2/all'
+
+    let f = fetch(url)
+    f.then((result) => {
+        return result.json()
+    }).then((data) => {
+        layout(data)
+    }).catch((err) => {
+        console.log(err)
+    })
+}
+makelayout()
+
+let layout = (x) => {
+    var m = 0
+    var sect = document.getElementById('containerSect')
+    for (let i = 1; i <= 63; i++) {
+        var r = document.createElement("div");
+        sect.appendChild(r);
+        r.className = "row";
+        r.id = `row${i}`;
+        var rid = document.getElementById(`row${i}`)
+
+        var br = document.createElement('br')
+        sect.appendChild(br)
+        for (let j = 0; j < 4; j++) {
+            var c = document.createElement("div");
+            rid.appendChild(c);
+            c.className = "col-lg-3 col-md-6 col-sm-12";
+            c.id = `col${j}`;
+            var cid = document.querySelector(`#row${i} #col${j}`)
+
+            cid.innerHTML = `
+            <div class="card" style="width: 16rem; background-color:#eeeeee; margin-bottom: 10px">
+                <div class="card-body" style = "padding: 20px 0">
+                    <div class ="padder">
+                        <h5 class="card-title">${x[m].name}</h5>
+                    </div>
+                    <img src=${x[m].flag} class="card-img-top" alt="..." style = "width:100%;height: 10rem; margin-bottom: 10px">
+                </div>
+                <div class="card-body" style = "padding: 0 15px 10px 15px">
+                    <h5 class="card-title">Capital: ${x[m].capital}</h5>
+                    <p class="card-text">Country Codes: ${x[m].alpha2Code} , ${x[m].alpha3Code}</p>
+                    <p class="card-text">Region: ${x[m].region}</p>
+                </div>
+                <div class="card-body buttons">
+                    <button type="button" id=${x[m].name} class="btn btn-primary" onclick ="getWeatherDetails(this.id)">Weather Details</button>
+                </div>
+          </div>`
+            m++
+        }
+    }
+}
+
+
+let getWeatherDetails = (y) => {
+    var api_key = 'eb8ea9f133d75bd85238f7fc85a99b09'
+    var weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${y}&appid=${api_key}`
+
+    var w = fetch(weatherUrl)
+
+    w.then((resultw) => {
+        return resultw.json()
+    }).then((dataw) => {
+        var buttonReplace = document.querySelector(`#${y}`)
+        buttonReplace.innerHTML = `Temperature is = ${(dataw.main.temp -273.15).toFixed(1)} &#8451`
+    }).catch((err) => {
+        console.log(err)
+    })
+}
